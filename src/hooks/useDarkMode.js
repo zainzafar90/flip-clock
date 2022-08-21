0f515501f868146a9db6b0449dc65db31a764919
Zain Zafar <zain@polly.ai>
2022-08-21 20:03:29 +0500

    📦 NEW: save user preference of light/dark mode in localStorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,6 +3,12 @@ import { useEffect, useState } from 'react'
 export const useDarkMode = () => {
   const [mode, setMode] = useState()
 
+  useEffect(() => {
+    if (mode) {
+      setPreferrence(mode)
+    }
+  }, [mode])
+
   /** since useEffect only runs on server side hence we don't need to check if `isBrowser()` */
   useEffect(() => {
     // Add listener to update styles
@@ -10,12 +16,21 @@ export const useDarkMode = () => {
       .matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', (e) => setMode(e.matches ? 'dark' : 'light'))
 
-    // Setup dark/light mode for the first time
-    setMode(
-      window.matchMedia('(prefers-color-scheme: dark)').matches
+    const userMode = localStorage.getItem('mode')
+    if (userMode) {
+      setMode(userMode)
+      setPreferrence(userMode)
+    } else {
+      console.log('HERE')
+      const systemMode = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
         ? 'dark'
         : 'light'
-    )
+
+      // Setup dark/light mode for the first time
+      setMode(systemMode)
+      setPreferrence(systemMode)
+    }
 
     // Remove listener
     return () => {
@@ -23,7 +38,12 @@ export const useDarkMode = () => {
         .matchMedia('(prefers-color-scheme: dark)')
         .removeEventListener('change', () => {})
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return [mode, setMode]
+}
+
+const setPreferrence = (mode) => {
+  localStorage.setItem('mode', mode)
 }
